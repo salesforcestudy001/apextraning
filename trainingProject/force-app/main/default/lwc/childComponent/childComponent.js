6da494d551770f7d37000c2fef7f51e72c7361fc
Zhang <xiao.g.zhang@accenture.com>
2021-09-27 17:10:22 +0800

    JangHyo0927

---
+++ trainingProject/force-app/main/default/lwc/childComponent/childComponent.js
@@ -0,0 +1,6 @@
+import { LightningElement , api } from 'lwc';
+
+export default class ChildComponent extends LightningElement {
+    @api const02 ;
+    
+}
---
+++ trainingProject/force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+    
+export default class ParentComponent extends LightningElement {
+    consts01 = [
+        {id : '1', name : 'a', sex : '1'},
+        {id : '2', name : 'b', sex : '2'},
+        {id : '3', name : 'c', sex : '1'},
+    ];
+
+}
