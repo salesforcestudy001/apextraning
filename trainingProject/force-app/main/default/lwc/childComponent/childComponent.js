a8017668d7fd8cbda4c49cb486a6b49c87156420
Cui <yu.e.cui@accenture.com>
2021-09-27 16:47:06 +0800

    9/27

---
+++ trainingProject/force-app/main/default/lwc/childComponent/childComponent.js
@@ -0,0 +1,9 @@
+import { LightningElement,api} from 'lwc';
+
+export default class ChildComponent extends LightningElement {
+   /* @api Id;
+    @api Name;
+    @api title;
+    */
+    @api temp;
+}
---
+++ trainingProject/force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -0,0 +1,27 @@
+
+import { LightningElement } from 'lwc';
+
+export default class ParentComponent extends LightningElement {
+    
+    contacts=[
+        {
+            Id:1,
+            Name:'cui',
+            Title:'www',
+            
+        },
+        {
+            Id:2,
+            Name:'wang',
+            Title:'rrr',
+        },
+        {
+            Id:3,
+            Name:'li',
+            Title:'zzz',
+            
+        },
+    ];
+    
+    
+}
