8a480b3a4d958d8e62b8c56e3bc3bdeb8845ec06
Wang <shiyao.c.wang@accenture.com>
2021-09-27 18:03:31 +0800

    wsyhk -- 513

---
+++ trainingProject/force-app/main/default/lwc/parentComponentQu/parentComponentQu.js
@@ -0,0 +1,12 @@
+// import UserPreferencesShowEmailToExternalUsers from "@salesforce/schema/User.UserPreferencesShowEmailToExternalUsers";
+import { LightningElement } from 'lwc';
+
+export default class ParentComponent extends LightningElement {
+    cons = [
+        {id:'1',name:'a',phone:'110',email:'110@qq.com'},
+        {id:'2',name:'b',phone:'120',email:'120@qq.com'},
+        {id:'3',name:'c',phone:'130',email:'130@qq.com'}
+    ];
+
+    
+}
