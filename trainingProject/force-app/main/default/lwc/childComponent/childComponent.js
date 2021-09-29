a71436f873fa0eafbfbd08042e5c06153bf37e86
Wang <shiyao.b.wang@accenture.com>
2021-09-29 14:17:44 +0800

    1

--- trainingProject/force-app/main/default/lwc/childComponent/childComponent.js
+++ trainingProject/force-app/main/default/lwc/childComponent/childComponent.js
@@ -2,4 +2,9 @@ import { LightningElement,api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
     @api temp;
+    handlePlay() {
+        console.log(this.temp.Name);
+        this.temp.Name = "change";
+        // console.log("123");
+    }
 }
