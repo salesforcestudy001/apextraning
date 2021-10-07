099f06a7045297e50f960cce6e662d64d4da2eae
Cui <yu.e.cui@accenture.com>
2021-10-07 17:00:20 +0800

    10/7

---
+++ trainingProject/force-app/main/default/lwc/addContact/addContact.js
@@ -0,0 +1,48 @@
+import { LightningElement, track } from 'lwc';
+import { con } from 'c/con';
+
+const columns = [
+    { label: 'ID', fieldName: 'Id' },
+    { label: 'NAME', fieldName: 'Name' },
+    { label: 'TITLE', fieldName: 'Title'}
+];
+export default class AddContact extends LightningElement {
+    contacts=[
+        {
+            Id:1,
+            Name:'cui',
+            Title:'C++',
+        },
+        {
+            Id:2,
+            Name:'Daming',
+            Title:'java',
+        },
+        {
+            Id:3,
+            Name:'lingling',
+            Title:'salesforce',
+        },
+    ];
+    columns=columns;
+    addHandler(){
+        console.log("添加一条记录！");
+        let qs=this.template.querySelector("lightning-datatable");
+        var addRecord = con();
+        addRecord.Id=this.contacts.length+1;
+        this.contacts.push(addRecord);
+        qs.data=this.contacts;
+        
+
+    }
+    minHandler(){
+        console.log("删除一条记录");
+        let qs=this.template.querySelector("lightning-datatable");
+        if (this.contacts.length>3) {
+            this.contacts.pop();
+            qs.data=this.contacts;
+        }else{
+            alert('false');
+        }      
+    }
+}
