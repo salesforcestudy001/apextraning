0f4d1cb955116570fde8b83b0a8c914cd24d8592
Sun <chenyang.sun@accenture.com>
2021-10-15 14:26:15 +0800

    1014 homework use customUI error solved

--- trainingProject/force-app/main/default/lwc/secondLogin/secondLogin.js
+++ trainingProject/force-app/main/default/lwc/secondLogin/secondLogin.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-14 14:11:46
- * @LastEditTime: 2021-10-15 11:37:02
+ * @LastEditTime: 2021-10-15 14:22:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \trainingProject\force-app\main\default\lwc\firstLogin\firstLogin.js
@@ -25,6 +25,7 @@ export default class FirstLogin extends LightningElement {
         username;
         firstname;
         lastname;
+        Records=[];
         get roleoptions(){
             return [{ label: 'Developer', value: 'Developer' },
                     { label: 'Architect/CTO', value: 'Architect/CTO' },
@@ -32,7 +33,12 @@ export default class FirstLogin extends LightningElement {
                     { label: 'IT Manager/Executive', value: 'IT Manager/Executive' },
                     { label: 'Business/Executive', value: 'Business/Executive' }];
         }
-        @wire (getRecords)Records;
+        @wire (getRecords)
+        getuser({data}){
+            if (data) {
+                this.Records=this.Records.concat(data);
+            }   
+        }
         handlefirstname(event){
             event.target.setCustomValidity('');
             this.firstname=event.target.value;
@@ -69,24 +75,28 @@ export default class FirstLogin extends LightningElement {
         }
 
     handleSave(){
+        //console.log(JSON.stringify(this.Records));
         const inputField = this.template.querySelector('.username');
-        let emailrule='^(\w+((-\w+)|(\.\w+))*)\+\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$';
-        // if (this.Records) {
-        //     for (let index = 0; index < this.Records.data.length; index++) {
-        //         if (this.Records.data[index].Username__c==inputField.value) {
-        //             alert("重复的Username禁止注册");
-        //             return "重复的Username禁止注册";
-        //         } 
-        //     }
-        // }
-        // if(emailrule.test(this.email)==false){
-        //     alert("邮箱格式错误");
-        //     return "邮箱格式错误";
-        // }
-        // if(emailrule.test(this.username)==false){
-        //     alert("用户名格式错误");
-        //     return "用户名格式错误";
-        // }
+        let emailrule=/^([0-9A-Za-z\-_\.]+)@([0-9A-Za-z]+\.[A-Za-z]{2,3}(\.[A-Za-z]{2})?)$/g;
+        let usernamerule=/^([0-9A-Za-z\-_\.]+)@([0-9A-Za-z]+\.[A-Za-z]{2,3}(\.[A-Za-z]{2})?)$/g;
+        if (this.Records) {
+            for (let index = 0; index < this.Records.length; index++) {
+                if (this.Records[index].Username__c==inputField.value) {
+                    alert("重复的Username禁止注册");
+                    return "重复的Username禁止注册";
+                } 
+            }
+        }
+        //console.log(this.email);
+        if(!emailrule.exec(this.email)){
+            alert("邮箱格式错误");
+            return "邮箱格式错误";
+        }
+        if(!usernamerule.exec(this.username)){
+            console.log(this.username);
+            alert("用户名格式错误");
+            return "用户名格式错误";
+        }
         const fields = {Name:this.name,
                         Username__c:this.username,
                         Role__c:this.role,
@@ -98,28 +108,36 @@ export default class FirstLogin extends LightningElement {
                         First_Name__c:this.firstname};
         const recordInput = { apiName:'UserScy__c', fields };
         createRecord(recordInput)
-        .then(result => {
-            consol.log('1');
-            this.uid = result.id;
-            const inputFields = this.template.querySelectorAll(
-                'lightning-input'
-            );
+        .then(response  => {
+            //console.log(JSON.stringify(response));
+            this.uid = response.id;
+            const temprecord={Username__c:response.fields.Username__c.value,Id:response.id}
+            this.Records.push(temprecord);
+            //console.log(JSON.stringify(this.Records));
+            const inputFields = this.template.querySelectorAll('lightning-input');
+            const combofields =this.template.querySelectorAll('lightning-combobox');
             if (inputFields) {
                 inputFields.forEach(field => {
                     this.temp[field.fieldName]=field.value;
-                    field.reset();
+                    field.value='';
+                });
+            }
+            if (combofields) {
+                combofields.forEach(field => {
+                    this.temp[field.fieldName]=field.value;
+                    field.value='';
                 });
             }
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Success',
-                    message: 'User created',
+                    message: 'User '+response.fields.Username__c.value+' created',
                     variant: 'success',
                 }),
             );
         })
         .catch(error => {
-            console.log(JSON.stringify(error));
+            //console.log(JSON.stringify(error.body));
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Error creating record',
